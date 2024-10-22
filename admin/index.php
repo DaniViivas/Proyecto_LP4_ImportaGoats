e2209199fbfa4ab64d3724604d6d81545a7e31d6
DaniViivas <osmanvivasunah2022@gmail.com>
2024-10-21 21:10:35 -0600

    Parte Inicial
    Inicio del proyecto

---
+++ admin/index.php
@@ -0,0 +1,50 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/sweetalert2.css">
+	<link rel="stylesheet" href="css/material.min.css">
+	<link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+	<link rel="stylesheet" href="css/main.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="js/material.min.js" ></script>
+	<script src="js/sweetalert2.min.js" ></script>
+	<script src="js/jquery.mCustomScrollbar.concat.min.js" ></script>
+	<script src="js/main.js" ></script>
+</head>
+<body>
+	<div class="login-wrap cover">
+		<div class="container-login">
+			<p class="text-center" style="font-size: 80px;">
+
+				<img src="img/Logo_ImportaGoatsSvg.svg" alt="Logo ImportaGoats" style="width:300px; height: 200px;" /> 
+
+				
+
+
+			</p>
+			<p class="text-center text-condensedLight">Iniciar Sesion</p>
+			<form action="funciones/home.html" method="POST">
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				    <input class="mdl-textfield__input" type="text" id="userName">
+				    <label class="mdl-textfield__label" for="userName">Usuario</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				    <input class="mdl-textfield__input" type="password" id="pass">
+				    <label class="mdl-textfield__label" for="pass">Contraseña</label>
+				</div>
+				<button class="mdl-button mdl-js-button mdl-js-ripple-effect" style="color: #000000; margin: 0 auto; display: block;">
+					INGRESAR
+				</button>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
