a2c7f4b3c80b9c30ee9e7397498f68dd64eef42f
DaniViivas <osmanvivasunah2022@gmail.com>
2024-10-21 21:18:40 -0600

    Cambio de menu a funcion
    Se hizo un cambio del home para que el menu sea estatico

--- admin/funciones/Funciones.php
+++ admin/funciones/Funciones.php
@@ -1,7 +1,7 @@
-<!-- 
-* Copyright 2018 Carlos Eduardo Alfaro Orellana
-  https://www.youtube.com/c/CarlosAlfaro007
--->
+<?php
+function encabezado($title) { 
+    $html = '';
+    $html .='<!doctype html>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -14,13 +14,33 @@
 	<link rel="stylesheet" href="/admin/css/material-design-iconic-font.min.css">
 	<link rel="stylesheet" href="/admin/css/jquery.mCustomScrollbar.css">
 	<link rel="stylesheet" href="/admin/css/main.css">
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</head>';
+echo $html;
+}
+function piePagina() {
+
+    echo <<<EOT
+    
+    
+
+
+ </div>
+    </main>
+         </body>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
 	<script src="/admin/js/material.min.js" ></script>
 	<script src="/admin/js/sweetalert2.min.js" ></script>
 	<script src="/admin/js/jquery.mCustomScrollbar.concat.min.js" ></script>
 	<script src="/admin/js/main.js" ></script>
-</head>
+    </html>
+
+EOT;
+}
+
+function menuPrincipal() {
+    echo <<<EOT
+
 <body>
 	<!-- Notifications area -->
 	<section class="full-width container-notifications">
@@ -446,5 +466,8 @@
 			</div>
 		</section>
 	</section>
-</body>
-</html>
+
+EOT;
+}
+
+?>
