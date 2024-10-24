00f5c1a8d2e7f3508ef609d84007c4d30c0d504c
DaniViivas <osmanvivasunah2022@gmail.com>
2024-10-23 22:16:56 -0600

    Persona
    Models: Persona terminada

--- api/models/Persona.php
+++ api/models/Persona.php
@@ -68,31 +68,39 @@ public function crear(){
    
  // ACTUALIZAR UNA PERSONA
  public function actualizar() {
+    $query = "CALL sp_actualizar_persona(:p_id_persona_pk, :p_nombre, :p_apellido, :p_fecha_nacimiento, :p_telefono, :p_direccion";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(':p_id_persona_pk', $this->id_persona_pk);
+    $stmt->bindParam(':p_nombre', $this->nombre);
+    $stmt->bindParam(':p_apellido',$this->apellido);
+    $stmt->bindParam('p_direccion',$this->direccion); 
+    $stmt->bindParam('p_correo_electronico',$this->correo_electronico);
+    $stmt->bindParam('p_telefono',$this->telefono);
+    $stmt->bindParam('p_fecha_afiliacion',$this->fecha_afiliacion);
+    $stmt->bindParam('p_DNI',$this->DNI);
+    $stmt->bindParam('p_fecha_nacimiento',$this->fecha_nacimiento);
+    $stmt->bindParam('p_usuario',$this->usuario);
+    $stmt->bindParam('p_contraseña',$this->contraseña);
+    $stmt->bindParam('p_id_tipo_persona_fk',$this->id_tipo_persona_fk);
 
-   
-  $query = "CALL sp_actualizar_persona(:p_id_persona_pk, :p_nombre, :p_direccion, :p_correo_electronico, :p_telefono, :p_fecha_afiliacion, :p_DNI, :p_fecha_nacimiento, :p_usuario, :p_contraseña, :p_tipo_persona_fk);
-  $stmt = $this->conn->prepare($query);
-  $stmt->bindParam(':p_id_persona_pk', $this->id_persona_pk);
-  $stmt->bindParam(':p_nombre', $this->nombre);
-  $stmt->bindParam(':p_apellido',$this->apellido);
-  $stmt->bindParam('p_direccion',$this->direccion); 
-  $stmt->bindParam('p_correo_electronico',$this->correo_electronico);
-  $stmt->bindParam('p_telefono',$this->telefono);
-  $stmt->bindParam('p_fecha_afiliacion',$this->fecha_afiliacion);
-  $stmt->bindParam('p_DNI',$this->DNI);
-  $stmt->bindParam('p_fecha_nacimiento',$this->fecha_nacimiento);
-  $stmt->bindParam('p_usuario',$this->usuario);
-  $stmt->bindParam('p_contraseña',$this->contraseña);
-  $stmt->bindParam('p_id_tipo_persona_fk',$this->id_tipo_persona_fk);
-      
+    if ($stmt->execute()) {
+        return true;
+    }
+ }
 
-       if ($stmt->execute()) {
+ // ELIMINAR UNA PERSONA
+  public function eliminar($id) {
+        $query = "CALL sp_eliminar_persona(:p_id_persona_pk)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':p_id_persona_pk', $id);
+
+        if ($stmt->execute()) {
             return true;
         }
         return false;
-    
     }
-   
+
+  
 
 }
     
