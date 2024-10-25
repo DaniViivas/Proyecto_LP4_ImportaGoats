7e4bfe75592dbe3bdaea2f763fd13f90ae54820c
DaniViivas <osmanvivasunah2022@gmail.com>
2024-10-25 16:38:31 -0600

    CRUD PERSONA
    Crud persona hecho

--- api/controllers/Personacontroller.php
+++ api/controllers/Personacontroller.php
@@ -21,16 +21,21 @@ public function leer(){
 }
 
 //obtener las personas por ID
-public function obtener($id){
+public function obtener($id) {
     $stmt = $this->Persona->obtener($id);
-    $persona = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo json_encode($persona);
+    $Persona = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo json_encode($Persona);
 }
 
 //crear una persona 
+
+
+
 public function crear($data){
+
    $this->Persona->nombre= $data->nombre;
    $this->Persona->apellido= $data->apellido;
+   $this->Persona->genero= $data->genero;
    $this->Persona->direccion= $data->direccion;
    $this->Persona->correo_electronico= $data->correo_electronico;
    $this->Persona->telefono= $data->telefono;
@@ -38,46 +43,53 @@ public function crear($data){
    $this->Persona->DNI= $data->DNI;
    $this->Persona->fecha_nacimiento= $data->fecha_nacimiento;
    $this->Persona->usuario= $data->usuario;
-   $this->Persona->contraseña= $data->contraseña;
+   $this->Persona->contrasena= $data->contrasena;
    $this->Persona->id_tipo_persona_fk= $data->id_tipo_persona_fk;
 
-   if($this->Persona->crear()){
-    echo json_encode(array('message' => 'Persona Creada'));
-   }else{
-    echo json_encode(array('message' => 'Persona No Creada'));
-   }
-}   
+   if ($this->Persona->crear()) {
+    echo json_encode(["mensaje" => "Persona creada exitosamente."]);
+} else {
+    echo json_encode(["mensaje" => "Error al crear la persona."]);
+}
+}  
 
-//Actualizar persona
-public function actualizar($data){
+
+// ACTUALIZAR UNA PERSONA
+
+public function actualizar($data) {
     $this->Persona->id_persona_pk= $data->id_persona_pk;
     $this->Persona->nombre= $data->nombre;
     $this->Persona->apellido= $data->apellido;
+    $this->Persona->genero= $data->genero;
     $this->Persona->direccion= $data->direccion;
     $this->Persona->correo_electronico= $data->correo_electronico;
     $this->Persona->telefono= $data->telefono;
     $this->Persona->fecha_afiliacion= $data->fecha_afiliacion;
     $this->Persona->DNI= $data->DNI;
-    $this->Persona->fecha_nacimiento= $data->fecha_nacimiento;  
+    $this->Persona->fecha_nacimiento= $data->fecha_nacimiento;
     $this->Persona->usuario= $data->usuario;
-    $this->Persona->contraseña= $data->contraseña;
+    $this->Persona->contrasena= $data->contrasena;
     $this->Persona->id_tipo_persona_fk= $data->id_tipo_persona_fk;
+ 
+    if ($this->Persona->actualizar()) {
+     echo json_encode(["mensaje" => "Persona creada exitosamente."]);
+ } else {
+     echo json_encode(["mensaje" => "Error al crear la persona."]);
+ }
+ }
 
-    if($this->Persona->actualizar()){
-        echo json_encode(array('message' => 'Persona Actualizada'));
-       }else{
-        echo json_encode(array('message' => 'Persona No Actualizada'));
-       }
-}   
+
+  
+   
 
 //ELIMINAR PERSONA
-public function eliminar($data){
-    $this->Persona->id_persona_pk= $data->id_persona_pk;
-    if($this->Persona->eliminar()){
-        echo json_encode(array('message' => 'Persona Eliminada'));
-       }else{
-        echo json_encode(array('message' => 'Persona No Eliminada'));
-       }
+
+public function eliminar($id) {
+    if ($this->Persona->eliminar($id)) {
+        echo json_encode(["mensaje" => "Persona eliminada exitosamente."]);
+    } else {
+        echo json_encode(["mensaje" => "Error al eliminar la persona."]);
+    }
 }
 
 }
--- api/models/Persona.php
+++ api/models/Persona.php
@@ -6,6 +6,7 @@ private $conn;
 public $id_persona_pk;
 public $nombre;
 public $apellido;
+public $genero;
 public $direccion;
 public $correo_electronico;
 public $telefono;
@@ -13,7 +14,7 @@ public $fecha_afiliacion;
 public $DNI;
 public $fecha_nacimiento;
 public $usuario;
-public $contraseña;
+public $contrasena;
 public $id_tipo_persona_fk; 
 
 public function __construct($db){
@@ -30,62 +31,83 @@ public function leer(){
 
 // OBTENER UNA PERSONA POR ID
 public function obtener($id) {
-    $query = "CALL sp_obtener_persona(:p_id_persona_pk)";
+    $query = "CALL sp_obtener_personas(:p_id_persona_pk)";
     $stmt = $this->conn->prepare($query);
     $stmt->bindParam(':p_id_persona_pk', $id);
     $stmt->execute();
     return $stmt;
 }
 
-// CREAR UNA PERSONA
+// CREAR UNA PERSONA// CREAR UNA PERSONA
+// Método crear de la clase Persona
 public function crear(){    
-    $query = "CALL sp_crear_persona(:p_nombre, :p_apellido, :p_direccion, :p_correo_electronico, :p_telefono, :p_fecha_afiliacion, :p_DNI, :p_fecha_nacimiento, :p_usuario, :p_contraseña, :p_id_tipo_persona_fk)";    
+    $query = "CALL sp_crear_persona(:p_nombre, :p_apellido, :p_genero, :p_direccion, :p_correo_electronico, :p_telefono, :p_fecha_afiliacion, :p_DNI, :p_fecha_nacimiento, :p_usuario, :p_contrasena, :p_id_tipo_persona_fk)";    
     $stmt = $this->conn->prepare($query);
     $stmt->bindParam(':p_nombre', $this->nombre);
-    $stmt->bindParam(':p_apellido',$this->apellido);
-    $stmt->bindParam('p_direccion',$this->direccion); 
-    $stmt->bindParam('p_correo_electronico',$this->correo_electronico);
-    $stmt->bindParam('p_telefono',$this->telefono);
-    $stmt->bindParam('p_fecha_afiliacion',$this->fecha_afiliacion);
-    $stmt->bindParam('p_DNI',$this->DNI);
-    $stmt->bindParam('p_fecha_nacimiento',$this->fecha_nacimiento);
-    $stmt->bindParam('p_usuario',$this->usuario);
-    $stmt->bindParam('p_contraseña',$this->contraseña);
-    $stmt->bindParam('p_id_tipo_persona_fk',$this->id_tipo_persona_fk);
+    $stmt->bindParam(':p_apellido', $this->apellido);
+    $stmt->bindParam(':p_genero', $this->genero);
+    $stmt->bindParam(':p_direccion', $this->direccion); 
+    $stmt->bindParam(':p_correo_electronico', $this->correo_electronico);
+    $stmt->bindParam(':p_telefono', $this->telefono);
+    $stmt->bindParam(':p_fecha_afiliacion', $this->fecha_afiliacion);
+    $stmt->bindParam(':p_DNI', $this->DNI);
+    $stmt->bindParam(':p_fecha_nacimiento', $this->fecha_nacimiento);
+    $stmt->bindParam(':p_usuario', $this->usuario);
+    $stmt->bindParam(':p_contrasena', $this->contrasena);
+    $stmt->bindParam(':p_id_tipo_persona_fk', $this->id_tipo_persona_fk);
 
-    if ($stmt->execute()) {
+    try {
+        $stmt->execute();
         return true;
+    } catch (PDOException $e) {
+        // Manejo del error, puedes usar $e->getMessage() para obtener detalles del error
+        echo "Error al ejecutar la consulta: " . $e->getMessage();
+        return false;
     }
-    return false;
 }
+
+
+
+
+
 
 
    
  // ACTUALIZAR UNA PERSONA
  public function actualizar() {
-    $query = "CALL sp_actualizar_persona(:p_id_persona_pk, :p_nombre, :p_apellido, :p_fecha_nacimiento, :p_telefono, :p_direccion";
+    $query = "CALL sp_actualizar_personas(:p_id_persona_pk, :p_nombre, :p_apellido, :p_genero, :p_direccion, :p_correo_electronico, :p_telefono, :p_fecha_afiliacion, :p_DNI, :p_fecha_nacimiento, :p_usuario, :p_contrasena, :p_id_tipo_persona_fk)";
     $stmt = $this->conn->prepare($query);
     $stmt->bindParam(':p_id_persona_pk', $this->id_persona_pk);
     $stmt->bindParam(':p_nombre', $this->nombre);
-    $stmt->bindParam(':p_apellido',$this->apellido);
-    $stmt->bindParam('p_direccion',$this->direccion); 
-    $stmt->bindParam('p_correo_electronico',$this->correo_electronico);
-    $stmt->bindParam('p_telefono',$this->telefono);
-    $stmt->bindParam('p_fecha_afiliacion',$this->fecha_afiliacion);
-    $stmt->bindParam('p_DNI',$this->DNI);
-    $stmt->bindParam('p_fecha_nacimiento',$this->fecha_nacimiento);
-    $stmt->bindParam('p_usuario',$this->usuario);
-    $stmt->bindParam('p_contraseña',$this->contraseña);
-    $stmt->bindParam('p_id_tipo_persona_fk',$this->id_tipo_persona_fk);
+    $stmt->bindParam(':p_apellido', $this->apellido);
+    $stmt->bindParam(':p_genero', $this->genero);
+    $stmt->bindParam(':p_direccion', $this->direccion);
+    $stmt->bindParam(':p_correo_electronico', $this->correo_electronico);
+    $stmt->bindParam(':p_telefono', $this->telefono);
+    $stmt->bindParam(':p_fecha_afiliacion', $this->fecha_afiliacion);
+    $stmt->bindParam(':p_DNI', $this->DNI);
+    $stmt->bindParam(':p_fecha_nacimiento', $this->fecha_nacimiento);
+    $stmt->bindParam(':p_usuario', $this->usuario);
+    $stmt->bindParam(':p_contrasena', $this->contrasena);
+    $stmt->bindParam(':p_id_tipo_persona_fk', $this->id_tipo_persona_fk);
 
-    if ($stmt->execute()) {
+   
+    try {
+        $stmt->execute();
         return true;
+    } catch (PDOException $e) {
+        // Manejo del error, puedes usar $e->getMessage() para obtener detalles del error
+        echo "Error al ejecutar la consulta: " . $e->getMessage();
+        return false;
     }
- }
+}
+
+
+
 
  // ELIMINAR UNA PERSONA
   public function eliminar($id) {
-        $query = "CALL sp_eliminar_persona(:p_id_persona_pk)";
+        $query = "CALL sp_eliminar_personas(:p_id_persona_pk)";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':p_id_persona_pk', $id);
 
